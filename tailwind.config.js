fc70525bcc5023490cad6de54be0350afd28929d
Joan Antonio <86362503+joanvino@users.noreply.github.com>
2024-04-30 06:24:32 +0800

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        secondary: '#64748b',
+        dark: '#0f172a',
+        wow: '#60a5fa',
+        sky: '#0891b2'
+      },
+      // screens: {
+      //   '2xl': '1320px',
+      // },
+    },
+  },
+  plugins: [],
+};
